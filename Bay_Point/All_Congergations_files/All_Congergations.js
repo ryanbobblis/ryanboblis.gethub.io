577f9bc52b24671e49f788f934d0e249d55a884b
ryanboblis <ryanboblis@iMac.T-mobile.com>
2016-10-08 11:55:04 -0700

    BPL Website

---
+++ Bay_Point/All_Congergations_files/All_Congergations.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20161008
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWStrokeParts([{rect:new IWRect(-5,5,10,184),url:'All_Congergations_files/stroke_8.png'},{rect:new IWRect(-5,-5,10,10),url:'All_Congergations_files/stroke_9.png'},{rect:new IWRect(5,-5,470,10),url:'All_Congergations_files/stroke_10.png'},{rect:new IWRect(475,-5,11,10),url:'All_Congergations_files/stroke_11.png'},{rect:new IWRect(475,5,11,184),url:'All_Congergations_files/stroke_12.png'},{rect:new IWRect(475,189,11,10),url:'All_Congergations_files/stroke_13.png'},{rect:new IWRect(5,189,470,10),url:'All_Congergations_files/stroke_14.png'},{rect:new IWRect(-5,189,10,10),url:'All_Congergations_files/stroke_15.png'}],new IWSize(480,194)),stroke_0:new IWStrokeParts([{rect:new IWRect(-5,5,10,323),url:'All_Congergations_files/stroke.png'},{rect:new IWRect(-5,-5,10,10),url:'All_Congergations_files/stroke_1.png'},{rect:new IWRect(5,-5,518,10),url:'All_Congergations_files/stroke_2.png'},{rect:new IWRect(523,-5,10,10),url:'All_Congergations_files/stroke_3.png'},{rect:new IWRect(523,5,10,323),url:'All_Congergations_files/stroke_4.png'},{rect:new IWRect(523,328,10,11),url:'All_Congergations_files/stroke_5.png'},{rect:new IWRect(5,328,518,11),url:'All_Congergations_files/stroke_6.png'},{rect:new IWRect(-5,328,10,11),url:'All_Congergations_files/stroke_7.png'}],new IWSize(528,333))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('All_Congergations_files/All_CongergationsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixupAllIEPNGBGs();Widget.onload();fixAllIEPNGs('Media/transparent.gif');IMpreload('All_Congergations_files','shapeimage_3','0');IMpreload('All_Congergations_files','shapeimage_3','1');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ Bay_Point/English_Locusts_files/English_Locusts.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20161008
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_2:new IWEmptyStroke(),stroke_0:new IWStrokeParts([{rect:new IWRect(-5,5,10,323),url:'English_Locusts_files/stroke.png'},{rect:new IWRect(-5,-5,10,10),url:'English_Locusts_files/stroke_1.png'},{rect:new IWRect(5,-5,518,10),url:'English_Locusts_files/stroke_2.png'},{rect:new IWRect(523,-5,10,10),url:'English_Locusts_files/stroke_3.png'},{rect:new IWRect(523,5,10,323),url:'English_Locusts_files/stroke_4.png'},{rect:new IWRect(523,328,10,11),url:'English_Locusts_files/stroke_5.png'},{rect:new IWRect(5,328,518,11),url:'English_Locusts_files/stroke_6.png'},{rect:new IWRect(-5,328,10,11),url:'English_Locusts_files/stroke_7.png'}],new IWSize(528,333)),stroke_1:new IWStrokeParts([{rect:new IWRect(-5,5,10,184),url:'English_Locusts_files/stroke_8.png'},{rect:new IWRect(-5,-5,10,10),url:'English_Locusts_files/stroke_9.png'},{rect:new IWRect(5,-5,470,10),url:'English_Locusts_files/stroke_10.png'},{rect:new IWRect(475,-5,11,10),url:'English_Locusts_files/stroke_11.png'},{rect:new IWRect(475,5,11,184),url:'English_Locusts_files/stroke_12.png'},{rect:new IWRect(475,189,11,10),url:'English_Locusts_files/stroke_13.png'},{rect:new IWRect(5,189,470,10),url:'English_Locusts_files/stroke_14.png'},{rect:new IWRect(-5,189,10,10),url:'English_Locusts_files/stroke_15.png'}],new IWSize(480,194))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('English_Locusts_files/English_LocustsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');fixupAllIEPNGBGs();Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
